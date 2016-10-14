193495e5e7305dd7dd90f75c540a5bc2cb2604cf
caojuan <2922475655@qq.com>
2016-10-14 15:49:46 +0800

    改过之后：修改资源，实现跳转页面并且等待2s，在项目清单中删除M的入口

---
+++ app/src/main/java/cn/ucai/fulicenter/activity/Main2Activity.java
@@ -0,0 +1,40 @@
+package cn.ucai.fulicenter.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+public class Main2Activity extends AppCompatActivity {
+    private static long sleepTime=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start  = System.currentTimeMillis();
+                long costTime = System.currentTimeMillis()-start;
+                if(sleepTime-costTime>0){
+//耗时操作
+                    try {
+                        Thread.sleep(sleepTime-costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+
+
+            }
+        }).start();
+    }
+}
