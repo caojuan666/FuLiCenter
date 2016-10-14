6c7a5c536d4d582b46f4d1b8c47bf853173ebd49
caojuan <2922475655@qq.com>
2016-10-14 19:42:49 +0800

    添加工具类L，在MainActivity实现

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,7 +1,11 @@
-package cn.ucai.fulicenter;
+package cn.ucai.fulicenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("main","MainActivity.onCreate()");
+    }
+    public void onCheckedChange(View view){
+
+    }
+
+
+    public void onClick(View view) {
+
     }
 }
