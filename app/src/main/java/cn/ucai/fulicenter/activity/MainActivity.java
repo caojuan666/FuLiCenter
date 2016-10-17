5bb84951855fb4a7d68e5af62e0572357b980896
caojuan <2922475655@qq.com>
2016-10-17 12:00:30 +0800

    添加butterknife注入控件，添加底部菜单的点击互斥方法

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,26 +1,86 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.utils.L;
 
+import static android.R.attr.id;
+
 public class MainActivity extends AppCompatActivity {
+    int index;
+    RadioButton[] ra ;
+    @BindView(R.id.btnNewGoods)
+    RadioButton btnNewGoods;
+    @BindView(R.id.btnBoutique)
+    RadioButton btnBoutique;
+    @BindView(R.id.btnCategory)
+    RadioButton btnCategory;
+    @BindView(R.id.btnCollect)
+    RadioButton btnCollect;
+    @BindView(R.id.tvshop)
+    RadioButton tvshop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        L.i("main","MainActivity.onCreate()");
+        ButterKnife.bind(this);
+        L.i("main", "MainActivity.onCreate()");
+        intiView();
     }
-    public void onCheckedChange(View view){
+
+    private void intiView() {
+        ra = new RadioButton[5];
+        ra[0]=btnNewGoods;
+        ra[1]=btnBoutique;
+        ra[2]=btnCategory;
+        ra[3]=btnCollect;
+        ra[4]= tvshop;
 
     }
 
+    public void onCheckedChange(View view) {
 
-    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnNewGoods:
+                 index = 0;
+            break;
+            case R.id.btnBoutique:
+                index = 1;
+                break;
+            case R.id.btnCategory:
+                index = 2;
+                break;
+            case R.id.btnCollect:
+                index = 3;
+                break;
+            case R.id.tvshop:
+                index = 4;
+                break;
+
+
+        }
+        setRadioButtonStatus();
 
     }
+
+    private void setRadioButtonStatus() {
+        for(int i=0;i<ra.length;i++){
+            if(i==index){
+                ra[i].setChecked(true);
+            }else{
+                ra[i].setChecked(false);
+            }
+        }
+    }
+
+
+
 }
