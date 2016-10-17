54d1d0523494ea1d81dedd68237bf0b39998ac49
caojuan <2922475655@qq.com>
2016-10-17 19:46:48 +0800

    添加网络请求的工具类及application

---
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -0,0 +1,21 @@
+package cn.ucai.fulicenter;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/10/17 0017.
+ */
+
+public class FuLiCenterApplication  extends Application{
+    private static FuLiCenterApplication instance;
+    public FuLiCenterApplication(){
+        instance = this;
+    }
+    public static FuLiCenterApplication getInstance(){
+        if(instance==null){
+            instance= new FuLiCenterApplication();
+        }
+        return instance;
+
+    }
+}
