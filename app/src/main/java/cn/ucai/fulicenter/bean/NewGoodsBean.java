5eb689e823abf20d41c8c9b88070e356610b6089
caojuan <2922475655@qq.com>
2016-10-20 21:01:40 +0800

    修改实体类

--- app/src/main/java/cn/ucai/fulicenter/bean/NewGoodsBean.java
+++ app/src/main/java/cn/ucai/fulicenter/bean/NewGoodsBean.java
@@ -1,27 +1,28 @@
 package cn.ucai.fulicenter.bean;
 
-/**
- * Created by Administrator on 2016/10/13 0013.
- */
 
 public class NewGoodsBean {
 
     /**
-     * id : 731
-     * goodsId : 6895
-     * catId : 345
-     * goodsName : IRM05W 幻彩蛹变色鼠
-     * goodsEnglishName : i-rocks 艾芮克
-     * goodsBrief : 传说中IG战队用过的鼠标（啊，别问我IG是什么鬼，真的不知道！）。提供更高的精确度及细致度，USB高速侦测模组技术。3D多彩背光DC技术，可依你喜好设定所需颜色。即插即用，无需安装任何软件。
-     * shopPrice : ￥399
-     * currencyPrice : ￥414
+     * id : 1
+     * goodsId : 7672
+     * catId : 0
+     * goodsName : 趣味煮蛋模具
+     * goodsEnglishName : Kotobuki
+     * goodsBrief : 将煮好的鸡蛋放到模具中，扣好卡扣，把蛋模放冰水，耐心等上10分钟，就可以变化成各种各样的形状，宝宝看了说不定胃口大开！
+     * shopPrice : ￥110
+     * currencyPrice : ￥140
      * promotePrice : ￥0
-     * rankPrice : ￥414
+     * rankPrice : ￥0
      * isPromote : false
-     * goodsThumb : 201508/thumb_img/6895_thumb_G_1439355137855.jpg
-     * goodsImg : 201508/thumb_img/6895_thumb_G_1439355137855.jpg
-     * addTime : 1442419200000
-     * shareUrl : http://m.fulishe.com/item/6895
+     * goodsThumb : 201509/thumb_img/7672_thumb_G_1442389445719.jpg
+     * goodsImg : 201509/goods_img/7672_P_1442389445199.jpg
+     * colorId : 4
+     * colorName : 绿色
+     * colorCode : #59d85c
+     * colorUrl : 1
+     * addTime : 1442389445
+     * promote : false
      */
 
     private int id;
@@ -37,12 +38,11 @@ public class NewGoodsBean {
     private boolean isPromote;
     private String goodsThumb;
     private String goodsImg;
-    private long addTime;
-    private String shareUrl;
-    public int length;
-    public boolean getProperties;
-    private int[] properties;
-
+    private int colorId;
+    private String colorName;
+    private String colorCode;
+    private String colorUrl;
+    private String addTime;
 
     public int getId() {
         return id;
@@ -124,12 +124,12 @@ public class NewGoodsBean {
         this.rankPrice = rankPrice;
     }
 
-    public boolean isIsPromote() {
+    public boolean isPromote() {
         return isPromote;
     }
 
-    public void setIsPromote(boolean isPromote) {
-        this.isPromote = isPromote;
+    public void setPromote(boolean promote) {
+        this.isPromote = promote;
     }
 
     public String getGoodsThumb() {
@@ -148,25 +148,49 @@ public class NewGoodsBean {
         this.goodsImg = goodsImg;
     }
 
-    public long getAddTime() {
+    public int getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public String getColorUrl() {
+        return colorUrl;
+    }
+
+    public void setColorUrl(String colorUrl) {
+        this.colorUrl = colorUrl;
+    }
+
+    public String getAddTime() {
         return addTime;
     }
 
-    public void setAddTime(long addTime) {
+    public void setAddTime(String addTime) {
         this.addTime = addTime;
-    }
-
-    public String getShareUrl() {
-        return shareUrl;
-    }
-
-    public void setShareUrl(String shareUrl) {
-        this.shareUrl = shareUrl;
     }
 
     @Override
     public String toString() {
-        return "NewGoodsBean{" +
+        return "NewGoodBean{" +
                 "id=" + id +
                 ", goodsId=" + goodsId +
                 ", catId=" + catId +
@@ -177,13 +201,14 @@ public class NewGoodsBean {
                 ", currencyPrice='" + currencyPrice + '\'' +
                 ", promotePrice='" + promotePrice + '\'' +
                 ", rankPrice='" + rankPrice + '\'' +
-                ", isPromote=" + isPromote +
+                ", promote=" + isPromote +
                 ", goodsThumb='" + goodsThumb + '\'' +
                 ", goodsImg='" + goodsImg + '\'' +
-                ", addTime=" + addTime +
-                ", shareUrl='" + shareUrl + '\'' +
+                ", colorId=" + colorId +
+                ", colorName='" + colorName + '\'' +
+                ", colorCode='" + colorCode + '\'' +
+                ", colorUrl='" + colorUrl + '\'' +
+                ", addTime='" + addTime + '\'' +
                 '}';
     }
-
-
 }
