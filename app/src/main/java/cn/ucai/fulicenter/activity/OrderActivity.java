b4d74b09e8596b96ea440324d8d3d117e8d1efd1
caojuan <2922475655@qq.com>
2016-10-30 16:10:33 +0800

    處理并完成支付操作

--- app/src/main/java/cn/ucai/fulicenter/activity/OrderActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/OrderActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.EditText;
@@ -7,7 +8,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.pingplusplus.android.PingppLog;
+import com.pingplusplus.libone.PaymentHandler;
+import com.pingplusplus.libone.PingppOne;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,9 +34,9 @@ import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.ResultUtils;
 import cn.ucai.fulicenter.view.DisplayUtils;
 
-public class OrderActivity extends BaseActivty {
-    private static final String TAG=OrderActivity.class.getSimpleName();
-
+public class OrderActivity extends BaseActivty implements PaymentHandler {
+    private static final String TAG = OrderActivity.class.getSimpleName();
+    private static String URL = "http://218.244.151.190/demo/charge";
     @BindView(R.id.et_cart_username)
     EditText etCartUsername;
     @BindView(R.id.et_cart_username_phone)
@@ -51,6 +62,14 @@ public class OrderActivity extends BaseActivty {
         mContext = this;
         mList = new ArrayList<>();
         super.onCreate(savedInstanceState);
+        //设置需要使用的支付方式
+        PingppOne.enableChannels(new String[]{"wx", "alipay", "upacp", "bfb", "jdpay_wap"});
+
+        // 提交数据的格式，默认格式为json
+        // PingppOne.CONTENT_TYPE = "application/x-www-form-urlencoded";
+        PingppOne.CONTENT_TYPE = "application/json";
+
+        PingppLog.DEBUG = true;
     }
 
     @Override
@@ -102,30 +121,30 @@ public class OrderActivity extends BaseActivty {
     @OnClick(R.id.order_buy)
     public void checkOrder() {
         String receiverName = etCartUsername.getText().toString();
-        if(TextUtils.isEmpty(receiverName)){
+        if (TextUtils.isEmpty(receiverName)) {
             etCartUsername.setError("收貨人姓名不能為空");
             etCartUsername.requestFocus();
             return;
         }
         String moble = etCartUsernamePhone.getText().toString();
-        if(TextUtils.isEmpty(moble)){
+        if (TextUtils.isEmpty(moble)) {
             etCartUsernamePhone.setError("手機號碼不能為空");
             etCartUsernamePhone.requestFocus();
             return;
         }
-        if(!moble.matches("[\\d]{11}")){
+        if (!moble.matches("[\\d]{11}")) {
             etCartUsernamePhone.setError("手機號碼格式有無");
             etCartUsernamePhone.requestFocus();
             return;
         }
         String address = etCartUsernameStreetAddress.getText().toString();
-        if(TextUtils.isEmpty(address)){
+        if (TextUtils.isEmpty(address)) {
             etCartUsernameStreetAddress.setError("街道地址不能为空");
             etCartUsernameStreetAddress.requestFocus();
             return;
         }
         String area = spinOrderProvince.getSelectedItem().toString();
-        if(TextUtils.isEmpty(area)){
+        if (TextUtils.isEmpty(area)) {
             Toast.makeText(OrderActivity.this, "收穫地址不為空", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -133,9 +152,6 @@ public class OrderActivity extends BaseActivty {
 
     }
 
-    private void gotoStatements() {
-        L.e(TAG,"ranPrice="+ranPrice);
-    }
 
     //计算价格
     private void sumPrice() {
@@ -143,7 +159,7 @@ public class OrderActivity extends BaseActivty {
         if (mList != null && mList.size() > 0) {
             for (CartBean c : mList) {
                 for (String id : ids) {
-                    L.e(TAG,"oreder.id="+id);
+                    L.e(TAG, "oreder.id=" + id);
                     if (id.equals(String.valueOf(c.getId()))) {
 //                        cartIds += c.getId() + ",";
                         ranPrice += getPrice(c.getGoods().getRankPrice()) * c.getCount();
@@ -159,6 +175,50 @@ public class OrderActivity extends BaseActivty {
     private int getPrice(String price) {
         price = price.substring(price.indexOf("￥") + 1);
         return Integer.valueOf(price);
+    }
+
+    private void gotoStatements() {
+        L.e(TAG, "ranPrice=" + ranPrice);
+        // 产生个订单号
+        String orderNo = new SimpleDateFormat("yyyyMMddhhmmss")
+                .format(new Date());
+
+
+        // 构建账单json对象
+        JSONObject bill = new JSONObject();
+
+        // 自定义的额外信息 选填
+        JSONObject extras = new JSONObject();
+        try {
+            extras.put("extra1", "extra1");
+            extras.put("extra2", "extra2");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            bill.put("order_no", orderNo);
+            bill.put("amount", ranPrice*100);
+            bill.put("extras", extras);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //壹收款: 创建支付通道的对话框
+        PingppOne.showPaymentChannels(getSupportFragmentManager(), bill.toString(), URL, this);
+    }
+
+    @Override
+    public void handlePaymentResult(Intent data) {
+            if (data != null) {
+                /**
+                 * code：支付结果码  -2:服务端错误、 -1：失败、 0：取消、1：成功
+                 * error_msg：支付结果信息
+                 */
+                int code = data.getExtras().getInt("code");
+                L.e(TAG, "code:" + code);
+                String errorMsg = data.getExtras().getString("error_msg");
+        }
     }
 
 
